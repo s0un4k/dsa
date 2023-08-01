bcb9bef8a1e05a604f9532e35d150db1b5568642
Sounak Roy <141139767+s0un4k@users.noreply.github.com>
2023-08-01 21:02:21 +0530

    CP Template

---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+sys.setrecursionlimit(100000000)
+
+from sys import stdin, stdout
+from statistics import mean, median, mode, variance
+from math import pow, sqrt, gcd, lcm, factorial, perm, comb, ceil, floor, log, log2, log10
+from collections import Counter as ctr, deque as dq, defaultdict as dd, OrderedDict as od
+from bisect import bisect_left as bl, bisect_right as br
+
+_fflush = lambda: stdout.flush()
+_stdr = lambda: stdin.readline()
+_stdw  = lambda x: stdout.write(str(x))
+
+_str = lambda: input().strip()
+_int = lambda: int(input().strip())
+_flt = lambda: float(input().strip())
+
+_lst_str = lambda: list(input().strip().split())
+_lst_int = lambda: list(map(int,input().strip().split()))
+_lst_flt = lambda: list(map(float,input().strip().split()))
+
+MOD = 1000000007
+
+
+def solve():
+    pass
+
+
+if __name__ == "__main__":
+    T = _int()
+
+    while T > 0:
+        solve()
+        T -= 1
